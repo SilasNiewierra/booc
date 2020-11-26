523fbb2417e139c0b1b16c9a50a18dd40f73f625
Silas <niewierra.silas@gmail.com>
2020-11-26 08:20:50 +0100

    updated detail page structure and bucket button behaviour

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:boek/app_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hexcolor/hexcolor.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,53 +9,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final ThemeData _themeData = new ThemeData(
-    brightness: Brightness.light,
-    fontFamily: 'Roboto',
-    primaryColor: Colors.blue[100],
-    accentColor: Colors.pinkAccent,
-    buttonColor: Colors.pinkAccent,
-    textTheme: TextTheme(
-      // Title
-      headline1: TextStyle(
-          color: Colors.blueGrey[900],
-          fontSize: 40.0,
-          fontWeight: FontWeight.w900),
-      // Greetig Text
-      headline2: TextStyle(
-          color: Colors.blueGrey[900],
-          fontSize: 40.0,
-          fontWeight: FontWeight.w900),
-      // Section Text
-      headline3: TextStyle(
-          color: Colors.blueGrey[900],
-          fontSize: 25.0,
-          fontWeight: FontWeight.w900),
-      // Book Title
-      headline4: TextStyle(
-          color: Colors.blueGrey[900],
-          fontSize: 20.0,
-          fontWeight: FontWeight.w900),
-      // Subtitle Heading
-      subtitle1: TextStyle(
-          color: Colors.blueGrey[900],
-          fontSize: 20.0,
-          fontWeight: FontWeight.w900),
-      // Author
-      subtitle2: TextStyle(color: HexColor('#A6ABBC'), fontSize: 20.0),
-      // Description
-      bodyText1: TextStyle(
-        color: HexColor('#A6ABBC'),
-        fontSize: 20.0,
-      ),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //theme: _themeData,
       home: AppHandler(),
     );
   }
--- lib/screens/detail/components/body.dart
+++ lib/screens/detail/components/body.dart
@@ -25,187 +25,213 @@ class _BodyState extends State<Body> {
       child: Stack(
         children: [
           // Title and Author
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 50.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          _buildHeading(),
+          // Content
+          _buildContent(size),
+          // Cover Image
+          _buildCover(size),
+        ],
+      ),
+    ));
+  }
+
+  Widget _buildHeading() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 50.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.bookItem.author,
+            style: TextStyle(color: detailTextColor),
+          ),
+          Text(
+            widget.bookItem.title,
+            style: Theme.of(context)
+                .textTheme
+                .headline4
+                .copyWith(color: detailTextColor, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContent(Size size) {
+    return Container(
+      margin: EdgeInsets.only(top: size.height * 0.4),
+      padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 50.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25.0),
+          topRight: Radius.circular(25.0),
+        ),
+      ),
+      child: Container(
+        margin: EdgeInsets.only(top: size.height * 0.05),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  widget.bookItem.author,
-                  style: TextStyle(color: detailTextColor),
+                  "Category: " + widget.bookItem.category,
+                  style: Theme.of(context).textTheme.headline5,
                 ),
-                Text(
-                  widget.bookItem.title,
-                  style: Theme.of(context).textTheme.headline4.copyWith(
-                      color: detailTextColor, fontWeight: FontWeight.bold),
+                ValueListenableBuilder(
+                  valueListenable: widget.bookItem.like,
+                  builder: (BuildContext ctx, bool like, Widget wdg) {
+                    return IconButton(
+                      icon: Icon(
+                        like ? Icons.favorite : Icons.favorite_border,
+                        color: Theme.of(context).accentColor,
+                        size: 35.0,
+                      ),
+                      onPressed: () {
+                        widget.bookItem.updateLiked(!like);
+                      },
+                    );
+                  },
                 ),
               ],
             ),
-          ),
-          // Content
-          Container(
-            margin: EdgeInsets.only(top: size.height * 0.4),
-            padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 50.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(25.0),
-                topRight: Radius.circular(25.0),
+            // Description
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 50.0),
+              child: Text(
+                widget.dataBloc.descriptionDummy,
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1
+                    .copyWith(fontWeight: FontWeight.w100),
               ),
             ),
-            child: Container(
-              margin: EdgeInsets.only(top: size.height * 0.05),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Category: " + widget.bookItem.category,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      ValueListenableBuilder(
-                        valueListenable: widget.bookItem.like,
-                        builder: (BuildContext ctx, bool like, Widget wdg) {
-                          return IconButton(
-                            icon: Icon(
-                              like ? Icons.favorite : Icons.favorite_border,
-                              color: Theme.of(context).accentColor,
-                              size: 35.0,
-                            ),
-                            onPressed: () {
-                              widget.bookItem.updateLiked(!like);
-                            },
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 50.0),
-                    child: Text(
-                      widget.dataBloc.descriptionDummy,
-                      style: Theme.of(context)
-                          .textTheme
-                          .subtitle1
-                          .copyWith(fontWeight: FontWeight.w100),
+            // Bottom Buttons
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 30.0),
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    border: Border.all(
+                      color: Theme.of(context).accentColor,
                     ),
                   ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 30.0),
-                        height: 60,
-                        width: 60,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(18),
-                          border: Border.all(
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ),
-                        child: FlatButton(
+                  child: ValueListenableBuilder(
+                      valueListenable: widget.bookItem.bucketed,
+                      builder: (BuildContext ctx, bool marked, Widget wdg) {
+                        return FlatButton(
                           padding: EdgeInsets.zero,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(18)),
                           onPressed: () {
-                            widget.dataBloc.addBucketBook(widget.bookItem);
-                            createToast(context, "Added to you bucket list");
+                            if (marked) {
+                              widget.bookItem.updateBucketed(false);
+                              widget.dataBloc.removeBucketBook(widget.bookItem);
+                              createToast(
+                                  context, "Removed from your Bucket List");
+                            } else {
+                              widget.bookItem.updateBucketed(true);
+                              widget.dataBloc.addBucketBook(widget.bookItem);
+                              createToast(context, "Added to you Bucket List");
+                            }
                           },
                           child: Center(
                             child: Icon(
-                              Icons.add,
+                              marked ? Icons.remove : Icons.add,
                               color: Theme.of(context).accentColor,
-                              size: 40,
+                              size: 30,
                             ),
                           ),
-                        ),
-                      ),
-                      Expanded(
-                        child: SizedBox(
-                          height: 60,
-                          child: ValueListenableBuilder(
-                            valueListenable: widget.bookItem.read,
-                            builder: (BuildContext ctx, bool read, Widget wdg) {
-                              return FlatButton(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18)),
-                                color: Theme.of(context).accentColor,
-                                onPressed: () {
-                                  if (read) {
-                                    widget.bookItem.updateRead(false);
-                                    widget.dataBloc
-                                        .removeReadBook(widget.bookItem);
-                                    createToast(
-                                        context,
-                                        "Removed \"" +
-                                            widget.bookItem.title +
-                                            "\" from your read list");
-                                  } else {
-                                    widget.bookItem.updateRead(true);
-                                    widget.dataBloc
-                                        .addReadBook(widget.bookItem);
-                                    createToast(
-                                        context,
-                                        "Added \"" +
-                                            widget.bookItem.title +
-                                            "\" to your read list");
-                                  }
-                                },
-                                child: Text(
-                                  read
-                                      ? "Unread".toUpperCase()
-                                      : 'Read'.toUpperCase(),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline4
-                                      .copyWith(
-                                          color: detailTextColor,
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold),
-                                ),
-                              );
-                            },
+                        );
+                      }),
+                ),
+                Expanded(
+                  child: SizedBox(
+                    height: 60,
+                    child: ValueListenableBuilder(
+                      valueListenable: widget.bookItem.read,
+                      builder: (BuildContext ctx, bool read, Widget wdg) {
+                        return FlatButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18)),
+                          color: Theme.of(context).accentColor,
+                          onPressed: () {
+                            if (read) {
+                              widget.bookItem.updateRead(false);
+                              widget.dataBloc.removeReadBook(widget.bookItem);
+                              createToast(
+                                  context,
+                                  "Removed \"" +
+                                      widget.bookItem.title +
+                                      "\" from your read list");
+                            } else {
+                              widget.bookItem.updateRead(true);
+                              widget.dataBloc.addReadBook(widget.bookItem);
+                              createToast(
+                                  context,
+                                  "Added \"" +
+                                      widget.bookItem.title +
+                                      "\" to your read list");
+                            }
+                          },
+                          child: Text(
+                            read
+                                ? "Unread".toUpperCase()
+                                : 'Read'.toUpperCase(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline4
+                                .copyWith(
+                                    color: detailTextColor,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.bold),
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // Cover Image
-          Container(
-            margin: EdgeInsets.only(top: size.height * 0.15),
-            padding: EdgeInsets.symmetric(horizontal: 50.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  height: 350,
-                  width: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Hero(
-                      tag: widget.bookItem.uniqueId,
-                      child: Image.asset(
-                        'assets/images/' + widget.bookItem.imgUrl,
-                        fit: BoxFit.fill,
-                      ),
+                        );
+                      },
                     ),
                   ),
                 ),
               ],
             ),
-          )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCover(Size size) {
+    return Container(
+      margin: EdgeInsets.only(top: size.height * 0.15),
+      padding: EdgeInsets.symmetric(horizontal: 50.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            height: 350,
+            width: 250,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Hero(
+                tag: widget.bookItem.uniqueId,
+                child: Image.asset(
+                  'assets/images/' + widget.bookItem.imgUrl,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
-    ));
+    );
   }
 }
